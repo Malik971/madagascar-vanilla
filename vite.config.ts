3d8261a5c50e1dbeb59aab2f97a8366e33fba423
malik ibo <malik97un@gmail.com>
2024-11-10 16:11:22 +0100

    Update vite.config.js et .ts for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/madagascar-vanilla/', // Remplace 'madagascar-vanilla' par le nom exact de ton dépôt GitHub
   plugins: [react()],
 });
+
